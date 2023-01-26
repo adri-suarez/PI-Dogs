199048f1cfaf4b1339deca195d7aeb25eabda0a8
zevek <adris1337@gmail.com>
2023-01-26 00:33:18 -0300

    load DB when server starts

--- api/index.js
+++ api/index.js
@@ -17,12 +17,14 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const loadDb = require("./src/controllers/loadDb");
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(3001, async () => {
+    await loadDb();
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
