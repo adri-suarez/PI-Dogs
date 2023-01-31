c0bbfaa49285613e6d92457d7836529bb60b5c22
zevek <adris1337@gmail.com>
2023-01-31 04:17:26 -0300

    post validators

--- api/src/controllers/postValidators.js
+++ api/src/controllers/postValidators.js
@@ -3,13 +3,13 @@ const getAndFormatData = require("./getAndFormatData");
 async function postValidator(name, height, weight) {
   let validator;
 
-  if (name.length < 3) validator = { msg: "missing name" };
-  else if (!height) validator = { msg: "missing height" };
-  else if (!weight) validator = { msg: "missing weight" };
+  if (name.length < 3) validator = { msg: "name is missing" };
+  else if (!height) validator = { msg: "height is missing" };
+  else if (!weight) validator = { msg: "weight is missing" };
 
   let getData = await getAndFormatData();
   if (getData.find((dog) => dog.name === name))
-    validator = { msg: "dog already exsists!" };
+    validator = { msg: "the dog already exists!" };
 
   return validator;
 }
