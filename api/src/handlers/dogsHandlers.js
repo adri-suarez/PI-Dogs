911b9286e726f71378a3eaa97ad2ac8c2af2ac07
zevek <adris1337@gmail.com>
2023-02-02 13:44:32 -0300

    modularized handler

--- api/src/handlers/dogsHandlers.js
+++ api/src/handlers/dogsHandlers.js
@@ -1,17 +1,17 @@
-const getAndFormatData = require("../controllers/getAndFormatData");
-const postDogController = require("../controllers/postDogController");
+const format_all_data = require("../controllers/formatAllData");
+const create_dog = require("../controllers/postDogController");
 
 const getDogs = async (req, res) => {
   const { name } = req.query;
   try {
-    let data = await getAndFormatData();
+    let data = await format_all_data();
     if (name) {
       let findByName = data.filter((dog) =>
         dog.name.toLowerCase().includes(name.toLowerCase())
       );
       findByName.length
         ? res.send(findByName)
-        : res.status(400).json({ msg: "dog not found" });
+        : res.status(400).json({ msg: "Dog not found" });
     } else {
       res.status(200).send(data);
     }
@@ -23,11 +23,11 @@ const getDogs = async (req, res) => {
 const dogById = async (req, res) => {
   try {
     const { id } = req.params;
-    let data = await getAndFormatData();
+    let data = await format_all_data();
     let dogById = data.find((e) => e.id == id);
     dogById
       ? res.status(200).send([dogById])
-      : res.status(400).json({ msg: `no dog with the id: ${id}` });
+      : res.status(400).json({ msg: `No dog with the id: ${id}` });
   } catch (error) {
     console.log(error);
   }
@@ -35,7 +35,7 @@ const dogById = async (req, res) => {
 
 const postDog = async (req, res) => {
   try {
-    let newDog = await postDogController(req.body);
+    let newDog = await create_dog(req.body);
     newDog.msg ? res.status(400).json(newDog) : res.status(200).json(newDog);
   } catch (error) {
     console.log(error);
