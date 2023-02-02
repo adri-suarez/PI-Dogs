c0c3224c718269fbc0746d095754df4ed287abee
zevek <adris1337@gmail.com>
2023-02-02 13:45:26 -0300

    validationMiddleware added to post router

--- api/src/routes/dogsRouter.js
+++ api/src/routes/dogsRouter.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const { getDogs, dogById, postDog } = require("../handlers/dogsHandlers");
+const validationMiddleware = require("../middlewares/validationMiddleware");
 const dogsRouter = Router();
 
 dogsRouter.get("/", getDogs);
 
 dogsRouter.get("/:id", dogById);
 
-dogsRouter.post("/", postDog);
+dogsRouter.post("/", validationMiddleware, postDog);
 
 module.exports = dogsRouter;
