72b9d8c937fb0de15e83c05b91df54d55fd8e4aa
zevek <adris1337@gmail.com>
2023-02-04 08:24:50 -0300

    display components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Form from "./pages/Form/Form";
+import Detail from "./pages/Detail/Detail";
+import SearchBar from "./pages/SearchBar/SearchBar";
+import { Route } from "react-router-dom";
+import Landing from "./pages/Landing/Landing";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/detail" component={Detail} />
+      <Route exact path="/form" component={Form} />
+
     </div>
   );
 }
