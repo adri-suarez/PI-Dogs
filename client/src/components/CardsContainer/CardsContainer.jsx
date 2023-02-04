c50a617dccce0dd0020cd8966760b88dabdb362c
zevek <adris1337@gmail.com>
2023-02-04 08:24:04 -0300

    map

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import Card from "../Card/Card";
 
 function CardsContainer() {
+  const dogs = [{
+    "id": 221,
+    "name": "Shetland Sheepdog",
+    "height": "33 - 41cm",
+    "weight": "14kg",
+    "life_span": "12 - 14 years",
+    "temperament": [
+    {
+    "name": "Affectionate"
+    },
+    {
+    "name": "Lively"
+    },
+    {
+    "name": "Responsive"
+    },
+    {
+    "name": "Alert"
+    },
+    {
+    "name": "Loyal"
+    },
+    {
+    "name": "Reserved"
+    },
+    {
+    "name": "Playful"
+    },
+    {
+    "name": "Gentle"
+    },
+    {
+    "name": "Intelligent"
+    },
+    {
+    "name": "Active"
+    },
+    {
+    "name": "Trainable"
+    },
+    {
+    "name": "Strong"
+    }
+    ],
+    "image": "https://cdn2.thedogapi.com/images/rJa29l9E7.jpg",
+    "created": false
+    }]
   return (
-    <div>CardsContainer</div>
-  )
+    <div>
+      {dogs.map((dog, index) => <Card dog={dog} id={index} />
+      )}
+    </div>
+  );
 }
 
-export default CardsContainer
+export default CardsContainer;
