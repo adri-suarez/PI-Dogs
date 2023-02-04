3b4744f77ee6fed4e873d545bba51008f0c24d77
zevek <adris1337@gmail.com>
2023-02-04 08:23:36 -0300

    Card test is working

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function Card() {
+function Card(props) {
   return (
-    <div>Card</div>
-  )
+    <>
+      <h2>{props.dog.name}</h2>
+      <img style={{ width: "200px" }} src={props.dog.image} alt="nodog" />
+      <div>{props.dog.height}</div>
+      <div>{props.dog.weight}</div>
+      <div>{props.dog.life_span}</div>
+      <hr />
+    </>
+  );
 }
 
-export default Card
+export default Card;
