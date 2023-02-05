1ca1fb7370d33859b2de3cabe324c7d4aad71a0f
zevek <adris1337@gmail.com>
2023-02-05 05:04:28 -0300

    backup

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import styles from './Card.module.css'
 
 function Card(props) {
   return (
-    <>
+    
+    <div className={styles.card}>
       <h2>{props.dog.name}</h2>
-      <img style={{ width: "200px" }} src={props.dog.image} alt="nodog" />
-      <div>{props.dog.height}</div>
-      <div>{props.dog.weight}</div>
+      <img src={props.dog.image} alt="nodog" />
+      <h4>Tamaño: {props.dog.height}</h4>
+        <h4>Peso: {props.dog.weight}</h4> 
       <div>{props.dog.life_span}</div>
-      <hr />
-    </>
+    </div>
+    
   );
 }
 
--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "./Landing.module.css";
+import Logo from "../../assets/images/Logo.png";
+import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div>Landing</div>
-  )
+    <div className={styles.landing}>
+      <br />
+      <h1>¡Bienvenido a mi Proyecto Individual!</h1>
+      <img src={Logo} alt="Logo" />
+      <br />
+      <Link to="/home">
+        <h3>Click here to continue</h3>
+      </Link>
+    </div>
+  );
 }
 
-export default Landing
+export default Landing;
