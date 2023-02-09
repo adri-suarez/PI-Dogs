a13407f2b964d2029e4d8fd03d1dffed875c9a7a
zevek <adris1337@gmail.com>
2023-02-09 10:24:00 -0300

    useDispatch added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import CardsContainer from "../../components/CardsContainer/CardsContainer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getDogs } from "../../redux/actions";
 
 function Home() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getDogs());
+  }, []);
+
   return <CardsContainer />;
 }
 
