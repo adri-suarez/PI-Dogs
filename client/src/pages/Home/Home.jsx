4ca7cd8a101735a6ad5d552e89a1dc1e3e0b91e3
zevek <adris1337@gmail.com>
2023-02-10 13:01:32 -0300

    just imports

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import CardsContainer from "../../components/CardsContainer/CardsContainer";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { getDogs } from "../../redux/actions";
+import { getDogs } from "../../redux/actions/actions";
 
 function Home() {
   const dispatch = useDispatch();
