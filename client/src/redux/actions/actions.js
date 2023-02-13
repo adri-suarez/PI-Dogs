a453de90431e59ac0fb778eded38f241d6a5d505
zevek <adris1337@gmail.com>
2023-02-13 01:19:23 -0300

    getTemps action

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_DOGS } from "./types";
+import { GET_DOGS, GET_TEMPS } from "./types";
 
 export const getDogs = () => {
   return async function (dispatch) {
@@ -9,4 +9,12 @@ export const getDogs = () => {
       payload: apiData.data,
     });
   };
+};
+
+export const getTemps = () => {
+  return function (dispatch){
+    fetch("http://localhost:3001/temperaments")
+    .then((res) => res.json())
+    .then((data) => dispatch({ type: GET_TEMPS, payload: data }));
+  }
 };
