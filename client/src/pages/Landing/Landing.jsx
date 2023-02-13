2a0ef4321cd5b54824acb7c990ad039dc2b29cbc
zevek <adris1337@gmail.com>
2023-02-13 01:20:06 -0300

    fixing styles

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -2,20 +2,14 @@ import React from "react";
 import styles from "./Landing.module.css";
 import Logo from "../../assets/images/Logo.png";
 import { Link } from "react-router-dom";
-import patihueso from "../../assets/images/patihueso.gif";
 
 function Landing() {
   return (
     <div className={styles.landing}>
-      <hr />
-      <h1>¡Bienvenido a mi Proyecto Individual!</h1>
       <img className={styles.logo} src={Logo} alt="Logo" />
       <Link to="/home">
-        <a href="/home">Click here to continue</a>
+        <p className={styles.continue}>Click here to continue</p>
       </Link>
-      <hr />
-      <img src={patihueso} className={styles.gif} alt="gif" />
-      <img src={patihueso} className={styles.gif} alt="gif" />
     </div>
   );
 }
