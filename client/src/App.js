d28749d6517dea85ba8afea6264551f7b05ff629
zevek <adris1337@gmail.com>
2023-02-14 08:38:12 -0300

    navBar removed to display in components

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import Form from "./pages/Form/Form";
 import Detail from "./pages/Detail/Detail";
-import NavBar from "./pages/NavBar/NavBar";
 import { Route } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <Route exact path="/" component={Landing} />
       <Route exact path="/home" component={Home} />
       <Route path="/dog/:id" component={Detail} />
