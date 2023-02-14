b8f9ad9319057abd470fb58bfd954f5925b63fc1
zevek <adris1337@gmail.com>
2023-02-14 16:50:48 -0300

    height and weight validations added

--- api/src/middlewares/validationMiddleware.js
+++ api/src/middlewares/validationMiddleware.js
@@ -1,14 +1,24 @@
 const format_all_data = require("../controllers/formatAllData");
 
 async function validationMiddleware(req, res, next) {
-  const { name, height, weight,life_span, temperament } = req.body;
-  //let validator;
-  if (!name) return res.status(400).json({ msg: "Name is missing" });
-  if (name.length < 3) return res.status(400).json({ msg: "Name is too short" });
-  if (!height) return res.status(400).json({ msg: "Height is missing" });
-  if (height > 199 || height < 10 || !Number(height)) return res.status(400).json({ msg: "Height is not real" });
-  if (!weight) return res.status(400).json({ msg: "Weight is missing" });
-  if (weight > 99 || weight < 1 || !Number(weight)) return res.status(400).json({ msg: "Weight is not real" });
+  const { name, weightMin, weightMax,heightMin, heightMax, life_span, temperament } = req.body;
+
+  if (!name) return res.status(400).json({ msg: "Name is missing!" });
+  if (name.length < 3) return res.status(400).json({ msg: "The name is too short" });
+  
+  if (weightMin > 199 || weightMin < 1 || !Number(weightMin)) return res.status(400).json({ msg: "Weight minimum is not real" });
+  if (weightMax > 199 || weightMax < 1 || !Number(weightMax)) return res.status(400).json({ msg: "Weight max is not real" });
+  if (heightMin > 129 || heightMin < 1 || !Number(heightMin)) return res.status(400).json({ msg: "Height minimum is not real" });
+  if (heightMax > 129 || heightMax < 1 || !Number(heightMax)) return res.status(400).json({ msg: "Height max is not real" });
+  
+  if(weightMax<weightMin) return res.status(400).json({msg:"The maximum weight cannot be lower than the minimum weight!"})
+  if(heightMax<heightMin) return res.status(400).json({msg:"The maximum height cannot be lower than the minimum height!"})
+  
+  if(!weightMin) return res.status(400).json({msg:"Minimum weight is missing!"})
+  if(!weightMax) return res.status(400).json({msg:"Max weight is missing!"})
+  if(!heightMin) return res.status(400).json({msg:"Minimum height is missing!"})
+  if(!heightMax) return res.status(400).json({msg:"Max height is missing!"})
+  
   if (life_span > 35 || life_span < 5 || !Number(life_span)) return res.status(400).json({ msg: "Life span is not real" });
   if (!temperament || temperament.length < 1) return res.status(400).json({ msg: "At least one temperament is required" });
 
